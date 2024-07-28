0611ace66a628678ea056225223d4b8fe48a5e1a
suryakanth-pillalamarri <suryakanthkrishna1@gmail.com>
2024-07-28 15:49:21 +0530

    submit and refresh buttons are added

--- genaipoc.py
+++ genaipoc.py
@@ -5,26 +5,53 @@ from langchain.schema import HumanMessage,SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
 
+#initiaizing the output parser
 parser=StrOutputParser()
+
+#loading environment variables from the environment variables
+#getting groq api key
 load_dotenv()
 groq_api_key=os.getenv("GROQ_API_KEY")
 
+#setting the title to the streamlit app
 st.title("GenAI POC SOP creater")
 
-input_text=st.text_input("Enter your prompt below")
+input_text=st.text_area("Enter your prompt below")
 
-if input_text:
+# Arrange buttons side by side using columns
+col1, col2 = st.columns([1, 1])
+with col1:
+    submit_button = st.button(label='Submit')
+with col2:
+    refresh_button = st.button(label='Refresh Input')
+
+
+
+#if input text is provided by the user
+if submit_button and input_text:
+    #initializing the Chatgroq model with the specified model id and api key
+    #here i am selecting the Gemma-2b-It
     model=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+    #defining the messages for sending to the model
     messages=[
     SystemMessage(content="Create sop for the given prompt"),
-    HumanMessage(content="Troubleshooting windows 11 installation in Virtual macahine")
+    # HumanMessage(content="Troubleshooting windows 11 installation in Virtual macahine")
+    HumanMessage(content=input_text)
     ]
     # print(model.invoke(messages))
+    #invoking the model
     result=model.invoke(messages)
-
+    #displaying the result using the Streamlit in the website
     st.write(parser.invoke(result))
+    
+    
+    st.write("Input is taken")
 
 
+
+if refresh_button:
+    input_text=""
+    st.query_params.clear()
 
 
 
